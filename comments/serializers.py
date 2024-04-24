7f7b40aeec985e7efe0d154d314c21a11e2e7f8e
isabellaobrien <bellaobrien.it@gmail.com>
2024-04-24 23:04:35 +0000

    Create comment serializer

---
+++ comments/serializers.py
@@ -0,0 +1,21 @@
+from .models import Comment
+from rest_framework import serializers
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id', 'owner', 'profile_id', 'profile_image', 'content', 'created_at', 'updated_at', 'is_owner', 'story'
+        ]
+
+class CommentDetailSerializer(CommentSerializer):
+    story = serializers.ReadOnlyField(source='story.id')
