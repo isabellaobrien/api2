28e890519c1d38c542927c99442d1c1fbf1f6cbd
isabellaobrien <bellaobrien.it@gmail.com>
2024-04-24 09:37:47 +0000

    Create story app and model

---
+++ stories/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Story(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+    return f"{self.id} {self.title}"
+
+
