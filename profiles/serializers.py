e18c3853994a35f9914a26465dc3e155310d84b3
isabellaobrien <bellaobrien.it@gmail.com>
2024-04-24 21:51:40 +0000

    Create profile serializer

---
+++ profiles/serializers.py
@@ -0,0 +1,16 @@
+from .models import Profile
+from rest_framework import serializers
+
+class ProfileSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id', 'owner', 'name', 'about_me', 'image', 'created_at', 'updated_at'
+        ]
