77f65accda04cc6d87ddb785621fd23093ac83aa
isabellaobrien <bellaobrien.it@gmail.com>
2024-04-25 13:09:38 +0000

    Create follower serializer

---
+++ followers/serializers.py
@@ -0,0 +1,21 @@
+from .models import Follower
+from rest_framework import serializers
+from django.db import IntegrityError
+
+class FollowerSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    followed = serializers.ReadOnlyField(source='followed.username')
+
+    class Meta:
+        model = Follower
+        fields = [
+            'id', 'owner', 'created_at', 'followed'
+        ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'already following'
+            })
