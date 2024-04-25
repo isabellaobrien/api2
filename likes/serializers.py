4ba8c6a3a91f6ef0cef509aa46bb46708c62ffd9
isabellaobrien <bellaobrien.it@gmail.com>
2024-04-25 10:58:58 +0000

    Create like serializer

---
+++ likes/serializers.py
@@ -0,0 +1,20 @@
+from .models import Like
+from rest_framework import serializers
+from django.db import IntegrityError
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = [
+            'id', 'owner', 'created_at', 'story'
+        ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'duplicate like'
+            })
