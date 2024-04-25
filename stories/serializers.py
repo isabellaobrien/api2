7a857abea493c0ea0c673a84e3df0eab685f6ed1
isabellaobrien <bellaobrien.it@gmail.com>
2024-04-25 22:40:03 +0000

    Add extra fields and filters to stories

--- stories/serializers.py
+++ stories/serializers.py
@@ -1,12 +1,33 @@
 from rest_framework import serializers
 from .models import Story
+from likes.models import Like
 
 class StorySerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                 owner=user, story=obj
+            ).first()
+            return like.id if like else None
+        return None
 
     class Meta:
         model = Story
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'title', 'content', 'description'
+            'id', 'owner', 'created_at', 'updated_at', 'title', 'content', 'description', 'is_owner', 'profile_id',
+            'profile_image', 'like_id', 'likes_count', 'comments_count'
         ]
 
--- stories/views.py
+++ stories/views.py
@@ -1,15 +1,28 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from .models import Story
 from .serializers import StorySerializer
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from social.permissions import IsOwnerOrReadOnly 
+from django.db.models import Count
 
 
 class StoryList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = StorySerializer
-    queryset = Story.objects.all()
+    # queryset = Story.objects.all()
+    queryset = Story.objects.annotate(
+        likes_count = Count('like', distinct=True),
+        comments_count = Count('comment', distinct=True)
+    ).order_by('-created_at')
+
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
+        'likes__created_at',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -17,4 +30,8 @@ class StoryList(generics.ListCreateAPIView):
 class StoryDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = StorySerializer
-    queryset = Story.objects.all()
+    # queryset = Story.objects.all()
+    queryset = Story.objects.annotate(
+        likes_count = Count('like', distinct=True),
+        comments_count = Count('comment', distinct=True)
+    ).order_by('-created_at')
