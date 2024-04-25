d4fc9af11619dff2da588ca7f45ae9101c467c32
isabellaobrien <bellaobrien.it@gmail.com>
2024-04-25 22:53:27 +0000

    Add extra fields and filters to profiles

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,16 +1,32 @@
 from .models import Profile
 from rest_framework import serializers
+from followers.models  import Follower
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
+    story_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
+
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
+
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'name', 'about_me', 'image', 'created_at', 'updated_at', 'is_owner'
+            'id', 'owner', 'name', 'about_me', 'image', 'created_at', 'updated_at', 'is_owner',
+            'following_id', 'story_count', 'followers_count', 'following_count'
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,35 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from social.permissions import IsOwnerOrReadOnly 
+from django.db.models import Count
 
 
 class ProfileList(generics.ListAPIView):
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        story_count = Count('owner__story', distinct=True),
+        followers_count = Count('owner__followed', distinct=True),
+        following_count = Count('owner__following', distinct=True)
+    ).order_by('-created_at')
+
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'story_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        story_count = Count('owner__story', distinct=True),
+        followers_count = Count('owner__followed', distinct=True),
+        following_count = Count('owner__following', distinct=True)
+    ).order_by('-created_at')
