3f0b935fb86bc2b6e9a8334dc8e4523cb19a2eac
isabellaobrien <bellaobrien.it@gmail.com>
2024-05-16 10:49:02 +0000

    Create save model and app

---
+++ save/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+from stories.models import Story
+
+class Save(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    story = models.ForeignKey(Story, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'story']
+
+    def __str__(self):
+        return f'{self.owner} {self.story}'
+
+
+
